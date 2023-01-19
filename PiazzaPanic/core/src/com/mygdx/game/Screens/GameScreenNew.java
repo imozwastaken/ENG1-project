864c20f53eae0759158773cbf1a27e5cb1a12268
Oscar Gunn <oscargunn@yahoo.com>
2023-01-19 13:22:04 +0000

    Started adding support for individual characters

--- PiazzaPanic/core/src/com/mygdx/game/Screens/GameScreenNew.java
+++ PiazzaPanic/core/src/com/mygdx/game/Screens/GameScreenNew.java
@@ -4,9 +4,11 @@ package com.mygdx.game.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -18,10 +20,13 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.game.PiazzaPanic;
+
+import java.io.*;
 
 public class GameScreenNew implements Screen{
     PiazzaPanic game;
@@ -34,9 +39,16 @@ public class GameScreenNew implements Screen{
     OrthogonalTiledMapRenderer renderer;
     OrthographicCamera gameCam;
 
-    private TextureAtlas atlas;
-    Texture cookImage;
+    // graphics stuff
+    TextureAtlas atlas;
+    TextureAtlas atlasIdle;
+    Sprite bob;
+    String[] lines;
+    FileHandle charIdles;
+    Skin skin;
+    private Array<Sprite> idles;
     SpriteBatch batch;
+    // movement stuff
     Vector3 touchPos = new Vector3();
     Float speed = 3f;
     int selected = 0;
@@ -60,15 +72,22 @@ public class GameScreenNew implements Screen{
 
         atlas = new TextureAtlas("charAnimations.atlas");
 
-        cookImage = new Texture(Gdx.files.internal("testChar.png"));
+        TextureAtlas atlasIdle = new TextureAtlas(Gdx.files.internal("charIdle.txt"));
+        skin = new Skin();
+        skin.addRegions(atlasIdle);
+        bob = skin.getSprite("Bob");
+        charIdles = Gdx.files.internal("charIdle.txt");
+        lines = charIdles.readString().split("\n");
+
+        batch = new SpriteBatch();
+        cooks = new Array<Actor>();
+        spawnCooks();
+
         batch = new SpriteBatch();
         cooks = new Array<Actor>();
         spawnCooks();
     }
 
-    public TextureAtlas getAtlas(){
-        return atlas;
-    }
 
     @Override
     public void show() {
@@ -77,15 +96,18 @@ public class GameScreenNew implements Screen{
 
     private void spawnCooks(){
         for (int i = 0; i < 3; i++){
+            Sprite current = skin.getSprite(lines[i*7+6]);
+            System.out.println(current);
+            //idles.add(current);
             Actor cook = new Actor();
-            cook.setX(100 * i);
-            cook.setY(0);
+            cook.setX(110 * i);
+            cook.setY(240);
             cook.setWidth(16);
             cook.setHeight(23);
             cook.addListener(new InputListener(){
                 public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
                     return true;
-                };
+                }
             });
             cooks.add(cook);
             gameStage.addActor(cook);
@@ -105,9 +127,10 @@ public class GameScreenNew implements Screen{
         renderer.render();
 
         // draw the cooks
+        int index = 0;
         batch.begin();
         for (Actor cook : cooks) {
-            batch.draw(cookImage, cook.getX(), cook.getY(), 80, 115);
+            batch.draw(bob, cook.getX(), cook.getY(), 115, 160);
         }
         batch.end();
 
@@ -123,7 +146,6 @@ public class GameScreenNew implements Screen{
             touchPos.set(Gdx.input.getX(), game.GAME_HEIGHT - Gdx.input.getY(), 0);
             System.out.println(touchPos);
         }
-        int index = 0;
         for (Actor cook : cooks) {
             if (cooks.get(index).isTouchFocusTarget()) {
                 selected = index;
@@ -157,17 +179,17 @@ public class GameScreenNew implements Screen{
 
 
         // make sure the chef stays within the screen bounds (limit to kitchen area for main game)
-        if (cooks.get(selected).getX() < 0){
-            cooks.get(selected).setX(0);
+        if (cooks.get(selected).getX() < 8){
+            cooks.get(selected).setX(8);
         }
-        if (cooks.get(selected).getX() > game.GAME_WIDTH - 16){
-            cooks.get(selected).setX(game.GAME_WIDTH - 16);
+        if (cooks.get(selected).getX() > 565){
+            cooks.get(selected).setX(565);
         }
-        if (cooks.get(selected).getY() < 0) {
-            cooks.get(selected).setY(0);
+        if (cooks.get(selected).getY() < 100) {
+            cooks.get(selected).setY(100);
         }
-        if (cooks.get(selected).getY() > game.GAME_HEIGHT - 16) {
-            cooks.get(selected).setY(game.GAME_HEIGHT - 16);
+        if (cooks.get(selected).getY() > 320) {
+            cooks.get(selected).setY(320);
         }
     }
 
