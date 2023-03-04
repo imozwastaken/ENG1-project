20aeb8791ea044e886dc6b03120a91647bf26ac4
shsakib.sis820@gmail.com <shsakib.sis820@gmail.com>
2023-03-04 20:09:07 +0000

    initialized tests

---
+++ PiazzaPanic/tests/src/com/mygdx/tests/CookTest.java
@@ -0,0 +1,17 @@
+package com.mygdx.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+public class CookTest {
+
+    @Test
+    /**
+     * This test is here to make sure the testing environment is not broken.
+     */
+    public void alwaysTrueTest() {
+        assertTrue("This test should pass every time", true);
+    }
+}
