26aad30c747f1daa28bf6bf314bd121d1b8564a1
Jake Keast <JRKeast2003@outlook.com>
2023-03-08 13:58:02 +0000

    bug fix

---
+++ PiazzaPanic/core/src/com/mygdx/game/Clickables/StationSpeedPowerup.java
@@ -0,0 +1,28 @@
+package com.mygdx.game.Clickables;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.mygdx.game.Powerups.Powerups;
+import com.mygdx.game.Screens.GameScreen;
+
+public class StationSpeedPowerup {
+
+    ImageButton stationSpeedClickable;
+    public StationSpeedPowerup(Utils utils, final GameScreen screen, final Powerups powerups ) {
+        stationSpeedClickable = utils.createImageClickable(new Texture("stopwatch.png"), 24, 24);
+        stationSpeedClickable.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                powerups.setSpeedMultiplier(2);
+            }
+        });
+
+    }
+
+    public ImageButton getStationClickable() {
+        return stationSpeedClickable;
+    }
+
+}
---
+++ PiazzaPanic/core/src/com/mygdx/game/Food/FoodMenu.java
@@ -0,0 +1,32 @@
+package com.mygdx.game.Food;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
+
+import java.util.ArrayList;
+
+public class FoodMenu {
+
+    private final ArrayList<Order> orderOptions;
+
+    public FoodMenu() {
+        this.orderOptions = new ArrayList<>();
+
+        // Add order options here
+        this.orderOptions.add(new Order("burger", new Texture("orderBurger.png"), new Burger()));
+        this.orderOptions.add(new Order("salad", new Texture("orderSalad.png"), new Salad()));
+    }
+
+    public ArrayList<Order> getOrderOptions() {
+        return orderOptions;
+    }
+
+    public Order getRandomOrder() {
+        return orderOptions.get(MathUtils.random(0, 1));
+    }
+
+    // Uncomment when logic added for adding a baking station
+    public void addNewMenuItems() {
+        // orderOptions.add(new Order("pizza", new Texture("orderPizza.png"), new Pizza()));
+    }
+}
